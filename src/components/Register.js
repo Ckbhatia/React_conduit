80ef9fafff62cd73907901a0a3d2046a59a056e7
Ckbhatia <chetansain86@gmail.com>
2019-09-26 16:25:10 +0530

    feat: Add register component

---
+++ src/components/Register.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import '../App.scss';
+import { Link } from 'react-router-dom';
+import Header from './Header/Header';
+
+export default class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    handleInput = (evt) => {
+        const {name, value} = evt.target;
+        this.setState({
+            [name]: value
+        })
+    }
+  render() {
+      return (
+          <>
+          <Header />
+            <div className="registeration-form-container">
+                <div className="form-main-container center-child">
+                    <div className="form-heading-container text-center">
+                        <h1 className="form-heading">Sign Up</h1>
+                        <Link className="login-link" to="/login">Have an account?</Link>
+                    </div>
+                    <div className="form-container">
+                      {/* Todo: update the url */}
+                        <form action="/api/users" method="POST">
+                            <input 
+                                type="text" 
+                                name="username" 
+                                className="input" 
+                                placeholder="Username"
+                                onChange={this.handleInput}
+                            />
+                            <input 
+                                type="email" 
+                                name="email" 
+                                className="input" 
+                                placeholder="Email"
+                                onChange={this.handleInput}
+                            />
+                            <input 
+                                type="password" 
+                                name="password" 
+                                className="input" 
+                                placeholder="Password"
+                                onChange={this.handleInput}
+                            />
+                            <button className="submit-btn">Sign in</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+      </>
+      )
+  }
+}
