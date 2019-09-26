244734000a6adfa7961e0650315907227985ae07
Ckbhatia <chetansain86@gmail.com>
2019-09-26 11:38:28 +0530

    fix: change import app.css to scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
+      <Route exact path='/' component={MainPage} />
     </div>
   );
 }
