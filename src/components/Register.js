c6cea862e5859260b441c540a53c91b418d5878d
Ckbhatia <chetansain86@gmail.com>
2019-10-01 17:43:26 +0530

    feat: add user register functionality

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import '../App.scss';
-import { Link } from 'react-router-dom';
+import { Link, withRouter as Router } from 'react-router-dom';
 import Header from './Header/Header';
+import styled from 'styled-components';
 
-export default class Register extends Component {
+class Register extends Component {
     constructor() {
         super();
         this.state = {
             username: '',
             email: '',
-            password: ''
+            password: '',
+            isError: false
         }
     }
 
@@ -19,6 +21,35 @@ export default class Register extends Component {
             [name]: value
         })
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('https://conduit.productionready.io/api/users', {
+            method: 'POST',
+            body: JSON.stringify({'user': this.state}),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(res => {
+            if (res.ok) {
+                // Redirect the user to home page
+                return res.json();
+            }
+            else {
+                // Change the state to true for error
+                this.setState({isError: true})
+                throw new Error('There is an error')
+            }
+        })
+        .then(res => {
+            this.props.fetchUser(res.user.token);
+            this.props.history.push('/')
+        })
+        .catch(error => console.dir(error))                                     
+    }
+
   render() {
       return (
           <>
@@ -29,9 +60,15 @@ export default class Register extends Component {
                         <h1 className="form-heading">Sign Up</h1>
                         <Link className="login-link" to="/login">Have an account?</Link>
                     </div>
+                    {/* IF error is true */}
+                    {this.state.isError && 
+                        <div className='error-container'>
+                            <Span className="error-text">email or username is already taken</Span>
+                        </div>
+                    }
                     <div className="form-container">
                       {/* Todo: update the url */}
-                        <form action="/api/users" method="POST">
+                        <form className="form">
                             <input 
                                 type="text" 
                                 name="username" 
@@ -53,7 +90,7 @@ export default class Register extends Component {
                                 placeholder="Password"
                                 onChange={this.handleInput}
                             />
-                            <button className="submit-btn">Sign in</button>
+                            <button className="submit-btn" onClick={this.handleSubmit}>Sign in</button>
                         </form>
                     </div>
                 </div>
@@ -62,3 +99,9 @@ export default class Register extends Component {
       )
   }
 }
+
+export default Router( Register );
+
+const Span = styled.span`
+    color: red;
+`
