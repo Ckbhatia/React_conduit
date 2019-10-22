90aa1e3e7565a251601b67a6899f1adc53952966
Ckbhatia <chetansain86@gmail.com>
2019-10-22 21:14:10 +0530

    feat: add protected and unproted routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.scss';
-import { Route } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
+import React from "react";
+import "./App.scss";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Page/Home";
+import Register from "./components/Page/Register";
+import Login from "./components/Page/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <Route exact path='/' component={MainPage} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: null
+  };
+
+  /**
+   * Returns routes based on condition
+   * @param {string}
+   * @return {object}
+   */
+  Routes = user => {
+    // Protected Routes
+    if (user) {
+      return (
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      );
+    }
+    // Unprotected Routes
+    else {
+      return (
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/*TODO: Add last default Route for error 404 */}
+        {this.Routes(this.state.user)}
+      </div>
+    );
+  }
 }
 
 export default App;
